d66efbaed5350bca7d23467ce19f81840ee1d328
cadigalvince <vincecadigal@aclcbukidnon.com>
2023-03-21 16:33:02 +0800

    Item_Detail

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,10 +1,15 @@
-function ProductCard () {
-    return(
-      <>
-    
-    
-      </>
-    )
-    }
+function ProductCard(post) {
+  return (
+    <a href={"/product/" + postMessage.post.id} className>
+      <img scr={post.post.image[0]}></img>
+      <h2>{post.post.tile}</h2>
+      <h3>p {post.post.price}</h3>
+      <span className="discount"> -%{post.post.discount}
+      </span>
+      <hr />
+      <p>{post.post.descripion}</p>
 
-    export default ProductCard
+    </a>
+  )
+}
+export default ProductCard
